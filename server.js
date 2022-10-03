74b38d97a4c26f9b093d0ae6972985e00e3565d4
marekbyrtek <marek.byrtekk@gmail.com>
2022-10-03 16:03:25 +0200

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Connected to port ${port}`));
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
