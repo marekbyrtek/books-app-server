3ceb720cbddc78821b57b747767b530a23b559af
marekbyrtek <marek.byrtekk@gmail.com>
2022-10-04 12:53:16 +0200

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const { db } = require("./config/db");
+const { db } = require("./config/db");
 const app = express();
 app.use(express.json());
 app.use(cors());
